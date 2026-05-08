158ea3d65f023f51719e71dcda04a45a33d9a2e4
Your Name <you@example.com>
2026-05-08 16:20:39 -0300

    user in memory

--- src/main/java/leandro/online/library/config/SecurityConfiguration.java
+++ src/main/java/leandro/online/library/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,12 +21,33 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http.
                 csrf(AbstractHttpConfigurer::disable).
-                formLogin(Customizer.withDefaults()).
+                formLogin(configurer ->{
+                    configurer.loginPage("/login").permitAll();
+                }).
                 httpBasic(Customizer.withDefaults()).
                 authorizeHttpRequests(autorize ->{
                     autorize.anyRequest().authenticated();
                 })
         .build();
 
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails user = User.builder().
+                username("leandro").
+                password(encoder.encode( "12345678")).
+                roles("USER").
+                build();
+        UserDetails user2 = User.builder().
+                username("gustavo").
+                password(encoder.encode( "12345678")).
+                roles("ADMIN").
+                build();
+        return new InMemoryUserDetailsManager(user,user2);
     }
 }
